df8f8e1f6588369b4b6a95ce67638fe7a42e437c
Alla Oleksyn <allaoleksyn@gmail.com>
2023-05-27 16:18:20 +0300

    🐛 BUG: fix date order in custom OrderBy pipe

--- src/app/shared/pipes/orderBy.pipe.spec.ts
+++ src/app/shared/pipes/orderBy.pipe.spec.ts
@@ -4,12 +4,12 @@ import { OrderByPipe } from './orderBy.pipe';
 describe('Pipe: OrderBye', () => {
 	const pipe = new OrderByPipe();
 	const mockArrayAsc = [
-		{ date: '2016-05-31T02:02:36+00:00' },
-		{ date: '2017-09-28T04:39:24+00:00' },
+		{ date: '2016-05-31T02:02:36+00:00', id: '111' },
+		{ date: '2017-09-28T04:39:24+00:00', id: '222' },
 	];
 	const mockArrayDesc = [
-		{ date: '2017-09-28T04:39:24+00:00' },
-		{ date: '2016-05-31T02:02:36+00:00' },
+		{ date: '2017-09-28T04:39:24+00:00', id: '222' },
+		{ date: '2016-05-31T02:02:36+00:00', id: '111' },
 	];
 
 	it('create an instance', () => {
@@ -29,6 +29,23 @@ describe('Pipe: OrderBye', () => {
 
 	it('transforms "mockArrayAsc" to "mockArrayAssc"', () => {
 		expect(pipe.transform(mockArrayAsc, 'date')?.values).toBe(
+			mockArrayAsc.values
+		);
+	});
+	it('transforms "mockArrayDesc" to "mockArrayAsc"', () => {
+		expect(pipe.transform(mockArrayDesc, 'id', 'asc')?.values).toBe(
+			mockArrayAsc.values
+		);
+	});
+
+	it('transforms "mockArrayAsc" to "mockArrayDesc"', () => {
+		expect(pipe.transform(mockArrayAsc, 'id', 'desc')?.values).toBe(
+			mockArrayDesc.values
+		);
+	});
+
+	it('transforms "mockArrayAsc" to "mockArrayAssc"', () => {
+		expect(pipe.transform(mockArrayAsc, 'id')?.values).toBe(
 			mockArrayAsc.values
 		);
 	});
--- src/app/shared/pipes/orderBy.pipe.ts
+++ src/app/shared/pipes/orderBy.pipe.ts
@@ -8,6 +8,18 @@ export class OrderByPipe implements PipeTransform {
 		if (!Array.isArray(value)) {
 			return;
 		}
+		if (field === 'date') {
+			return value.sort((a: any, b: any) => {
+				if (order === 'asc') {
+					return new Date(a[field]).getTime() - new Date(b[field]).getTime();
+				}
+				if (order === 'desc') {
+					return new Date(b[field]).getTime() - new Date(a[field]).getTime();
+				} else {
+					return 0;
+				}
+			});
+		}
 		return value.sort((a: any, b: any) => {
 			if (order === 'asc') {
 				return -1;
