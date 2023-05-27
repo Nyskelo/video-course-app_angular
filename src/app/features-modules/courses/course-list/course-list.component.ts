be55b988e7c4e8cd9bb50e09561f4571e0d0e8b8
Alla Oleksyn <allaoleksyn@gmail.com>
2023-05-27 16:17:43 +0300

    🎨 IMPROVE: moved custom Filter Pipe to component

--- src/app/features-modules/courses/course-list/course-list.component.ts
+++ src/app/features-modules/courses/course-list/course-list.component.ts
@@ -3,9 +3,14 @@ import {
 	Component,
 	EventEmitter,
 	Input,
+	OnChanges,
+	OnInit,
 	Output,
+	SimpleChanges,
 	ViewEncapsulation,
 } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { FilterPipe } from 'src/app/shared/pipes/filter.pipe';
 import { Course } from 'src/app/utils/global.model';
 @Component({
 	selector: 'app-course-list',
@@ -14,10 +19,32 @@ import { Course } from 'src/app/utils/global.model';
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	encapsulation: ViewEncapsulation.None,
 })
-export class CourseListComponent {
+export class CourseListComponent implements OnInit, OnChanges {
+	constructor(private filterPipe: FilterPipe) {}
+
 	@Input() courses!: Course[];
 	@Input() filterBy!: string;
 	@Output() deleteCourse: EventEmitter<number> = new EventEmitter();
+
+	filteredCourses$ = new BehaviorSubject<Course[]>([]);
+
+	ngOnChanges(changes: SimpleChanges): void {
+		const updateCourses: Course[] = this.filterPipe.transform(
+			this.courses,
+			`${changes['filterBy']['currentValue']}`,
+			'name'
+		);
+		this.filteredCourses$.next(updateCourses);
+		console.log(updateCourses);
+	}
+	ngOnInit(): void {
+		const updateCourses = this.filterPipe.transform(
+			this.courses,
+			`${this.filterBy}`,
+			'name'
+		);
+		this.filteredCourses$.next(updateCourses);
+	}
 
 	trackCourseID(index: number, course: Course): number {
 		return course.id;
