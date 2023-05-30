620cef1adcc81f286841b53e35f6999856c97614
Alla Oleksyn <allaoleksyn@gmail.com>
2023-05-30 22:36:13 +0300

    🧪 TEST: login and header components spec covered

--- src/app/core/components/header/header.component.spec.ts
+++ src/app/core/components/header/header.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { HeaderComponent } from './header.component';
+import { AuthService } from '../../services/auth.service';
+
+let serviceSpy: jasmine.SpyObj<AuthService>;
+const user = { firstName: 'Pretty', lastName: 'Funny', id: '999' };
 
 describe('HeaderComponent', () => {
 	let component: HeaderComponent;
@@ -11,11 +15,24 @@ describe('HeaderComponent', () => {
 			schemas: [NO_ERRORS_SCHEMA],
 			declarations: [HeaderComponent],
 		});
+		serviceSpy = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
 		fixture = TestBed.createComponent(HeaderComponent);
 		component = fixture.componentInstance;
 	});
+	describe('onLogout', () => {
+		it('should call service method "logout"', () => {
+			spyOn(serviceSpy, 'logout').and.callThrough();
+			component.onLogout();
+			expect(serviceSpy.logout).toHaveBeenCalled();
+		});
+	});
 
-	it('can load instance', () => {
-		expect(component).toBeTruthy();
+	describe('userName', () => {
+		it(`should call service method "getUserInfo" and return ${user}`, () => {
+			spyOnProperty(component, 'userName', 'get').and.callThrough();
+			spyOn(serviceSpy, 'getUserInfo').and.returnValue(user);
+			expect(component.userName).toEqual(user.firstName);
+			expect(serviceSpy.getUserInfo).toHaveBeenCalled();
+		});
 	});
 });
--- src/app/features-modules/login/login.component.spec.ts
+++ src/app/features-modules/login/login.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { LoginComponent } from './login.component';
+import { AuthService } from 'src/app/core/services/auth.service';
+
+let serviceSpy: jasmine.SpyObj<AuthService>;
 
 describe('LoginComponent', () => {
 	let component: LoginComponent;
@@ -13,9 +16,54 @@ describe('LoginComponent', () => {
 		});
 		fixture = TestBed.createComponent(LoginComponent);
 		component = fixture.componentInstance;
+		serviceSpy = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
+		spyOn(window, 'alert');
 	});
 
-	it('can load instance', () => {
-		expect(component).toBeTruthy();
+	describe('onInputEmailValue', () => {
+		it('should emit logEmail', () => {
+			spyOn(component, 'onInputEmailValue').and.callThrough();
+			spyOn(component.logEmail, 'emit');
+			const newInputValue = 'new-email';
+			component.onInputEmailValue(newInputValue);
+			expect(component.logEmail.emit).toHaveBeenCalledWith(newInputValue);
+		});
+	});
+
+	describe('onInputPasswordValue', () => {
+		it('should emit logPassword', () => {
+			spyOn(component, 'onInputPasswordValue').and.callThrough();
+			spyOn(component.logPassword, 'emit');
+			const newInputValue = 'new-password';
+			component.onInputPasswordValue(newInputValue);
+			expect(component.logPassword.emit).toHaveBeenCalledWith(newInputValue);
+		});
+	});
+
+	describe('onSubmit', () => {
+		it('should not be called if any field is empty', () => {
+			spyOn(component, 'onSubmit').and.callThrough();
+			component.email = '';
+			component.password = '';
+			component.onSubmit();
+			expect(window.alert).toHaveBeenCalled();
+		});
+
+		it('should clear inputs and call the service login method if the data is valid', () => {
+			spyOn(component, 'onSubmit').and.callThrough();
+			spyOn(serviceSpy, 'login').and.returnValue();
+			spyOn(component.logEmail, 'emit');
+			spyOn(component.logPassword, 'emit');
+
+			const newUser = { firstName: 'Pretty', lastName: 'GoodDay', id: '111' };
+			component.email = 'not empty';
+			component.password = 'not empty';
+
+			component.onSubmit();
+
+			expect(component.logEmail.emit).toHaveBeenCalledWith('');
+			expect(component.logPassword.emit).toHaveBeenCalledWith('');
+			expect(serviceSpy.login).toHaveBeenCalledWith(newUser);
+		});
 	});
 });
