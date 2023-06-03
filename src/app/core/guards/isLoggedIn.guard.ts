15c7c867a2b982609efea8cfe2629e3b1f160711
Alla Oleksyn <allaoleksyn@gmail.com>
2023-06-03 13:45:45 +0300

    ✨FEATURE: added isLogginGuard, coursesResolver

--- src/app/core/guards/isLoggedIn.guard.ts
+++ src/app/core/guards/isLoggedIn.guard.ts
@@ -12,8 +12,6 @@ export class isLoggedInGuard {
 			this.router.navigate(['/login']);
 			return false;
 		}
-		// logged in, so return true
-		this.authService.isAuthenticated();
 		return true;
 	}
 }
