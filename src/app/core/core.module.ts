db8829ec54a9b3a9dae39d1cc426d128501fd9eb
Alla Oleksyn <allaoleksyn@gmail.com>
2023-06-08 20:58:12 +0300

    ✨FEATURE: added AuthTokenInterceptorService

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,22 @@
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { CoursesResolver } from './resolvers/courses.resolver';
+import { AuthTokenInterceptorService } from './services/auth-token-interceptor.service';
 import { AuthService } from './services/auth.service';
 
 @NgModule({
 	declarations: [],
 	imports: [SharedModule],
 	exports: [SharedModule],
-	providers: [AuthService, CoursesResolver],
+	providers: [
+		AuthService,
+		CoursesResolver,
+		{
+			provide: HTTP_INTERCEPTORS,
+			useClass: AuthTokenInterceptorService,
+			multi: true,
+		},
+	],
 })
 export class CoreModule {}
