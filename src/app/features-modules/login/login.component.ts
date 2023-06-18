7f69a72288d97e618e12ad4944117a287990f13c
Alla Oleksyn <allaoleksyn@gmail.com>
2023-06-18 15:09:40 +0300

    🔨 REFACTOR: login inputs to FormGroup

--- src/app/features-modules/login/login.component.ts
+++ src/app/features-modules/login/login.component.ts
@@ -1,15 +1,10 @@
-import {
-	ChangeDetectionStrategy,
-	Component,
-	computed,
-	OnDestroy,
-	signal,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
 import { UntilDestroy } from '@ngneat/until-destroy';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { Store } from '@ngrx/store';
 import * as UserActions from 'src/app/store/user/actions';
 import { AppStateInterface } from 'src/app/store';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @UntilDestroy({ checkProperties: true })
 @Component({
@@ -22,37 +17,33 @@ import { AppStateInterface } from 'src/app/store';
 })
 export class LoginComponent implements OnDestroy {
 	token = '';
-	constructor(private store: Store<AppStateInterface>) {
+	constructor(
+		private store: Store<AppStateInterface>,
+		private formBuilder: FormBuilder
+	) {
 		this.token = JSON.parse(localStorage.getItem('token') as string);
 	}
 	ngOnDestroy(): void {
 		console.log('LOGIN - LoginComponent has been destroyed');
 	}
-
-	email = signal('');
-	password = signal('');
-	authData = computed(() => {
-		return {
-			login: this.email(),
-			password: this.password(),
-		};
+	form = this.formBuilder.group({
+		email: ['', [Validators.required]],
+		password: ['', [Validators.required]],
 	});
-
-	onInputEmailValue(value: string) {
-		this.email.set(value);
+	get email() {
+		return this.form.get('email') as FormControl;
 	}
-	onInputPasswordValue(value: string) {
-		this.password.set(value);
+	get password() {
+		return this.form.get('password') as FormControl;
 	}
-
 	onSubmit() {
-		if (!this.email().trim() || !this.password().trim()) {
+		if (this.form.invalid) {
 			alert('Please complete the fields below');
 			return;
 		}
 		this.store.dispatch(
 			UserActions.userLogin({
-				auth: this.authData(),
+				auth: { login: this.email.value, password: this.password.value },
 			})
 		);
 	}
